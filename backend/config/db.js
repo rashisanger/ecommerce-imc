f0d84e4678891515207dcbb5b0ebc71bd8e3d586
Rashi <rashi.sanger09@gmail.com>
2025-12-22 16:02:50 +0530

    server

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,22 @@
 const mongoose = require("mongoose");
 
-let isConnected = false;
+let isConnected; // undefined initially
 
 const connectDB = async () => {
-  if (isConnected) return;
+  if (isConnected) {
+    console.log("✅ Using existing MongoDB connection");
+    return;
+  }
 
-  if (!process.env.MONGO_URI) throw new Error("MONGO_URI is not defined");
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is not defined");
+  }
 
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      bufferCommands: false
+      bufferCommands: false,
     });
+
     isConnected = conn.connections[0].readyState === 1;
     console.log("✅ MongoDB connected");
   } catch (err) {
